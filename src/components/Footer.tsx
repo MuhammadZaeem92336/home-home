e15d36febadf0cc10bf63c77e53d8f2d66c6f3c6
MUHAMMAD-ZAEEM <zaeem.elegantbiz@gmail.com>
2024-09-08 12:45:33 +0500

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="flex gap-9 p-10 bg-indigo-200">
             <Link href="\">Home</Link>
-            <Link href="about">About Home icon</Link>
+            <Link href="about">About Home</Link>
             <Link href="contact">About Contact</Link>
             <Link href="address">About Home Adress</Link>
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className="flex gap-9 p-10 bg-orange-200">
             <Link href="\">Home</Link>
-            <Link href="about">About Home icon</Link>
+            <Link href="about">About Home</Link>
             <Link href="contact">About Contact</Link>
             <Link href="address">About Home Adress</Link>
         </div>
