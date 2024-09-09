27e9609fe88cecafb1f79da9bb81cf40383220e4
MUHAMMAD-ZAEEM <zaeem.elegantbiz@gmail.com>
2024-09-09 20:43:26 +0500

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,11 @@ import React from "react"
 import Link from "next/link"
 const Footer = () => {
     return (
-        <div className="flex gap-9 p-10 bg-indigo-200">
-            <Link href="\">Home</Link>
-            <Link href="about">About Home</Link>
-            <Link href="contact">About Contact</Link>
-            <Link href="address">About Home Adress</Link>
+        <div className="text-emerald-900 flex gap-9 p-10 bg-indigo-200">
+            <Link href="\">┃𝑯𝒐𝒎𝒆┃</Link>
+            <Link href="about">┃𝑨𝒃𝒐𝒖𝒕 𝑯𝒐𝒎𝒆┃</Link>
+            <Link href="contact">┃𝑨𝒃𝒐𝒖𝒕 𝑪𝒐𝒏𝒕𝒂𝒄𝒕┃</Link>
+            <Link href="address">┃𝑨𝒃𝒐𝒖𝒕 𝑯𝒐𝒎𝒆 𝑨𝒅𝒅𝒓𝒆𝒔𝒔┃</Link>
         </div>
     )
 }
