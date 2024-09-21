a777c1d740e5496a55e2449a229bc07f2009bd93
MUHAMMAD-ZAEEM <zaeem.elegantbiz@gmail.com>
2024-09-21 21:54:29 +0500

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import Link from "next/link"
 const Footer = () => {
     return (
-        <div className="fixed bottom-0 flex gap-20 fix-bottom p-10 bg-indigo-200 w-full justify-center font-serif">
+        <div className="fixed bottom-0 flex gap-20 fix-bottom p-10 w-full justify-center font-serif bg-gradient-to-r from-indigo-400 from-% via-sky-500 via-30% to-emerald-500 to-100% ...">
             <Link href="\"><i><b>Home</b></i></Link>
             <Link href="about"><i><b>About Home</b></i></Link>
             <Link href="contact"><i><b>About Contact</b></i></Link>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import Link from "next/link"
 const Header = () => {
     return (
-        <div className="flex gap-20 p-10 bg-orange-200 justify-left font-serif">
+        <div className="flex gap-20 p-10 justify-left font-serif w-full bg-gradient-to-r from-cyan-500 to-blue-400 ...">
             <Link href="\"><i><b>Home</b></i></Link>
             <Link href="about"><i><b>About Home</b></i></Link>
             <Link href="contact"><i><b>About Contact</b></i></Link>
